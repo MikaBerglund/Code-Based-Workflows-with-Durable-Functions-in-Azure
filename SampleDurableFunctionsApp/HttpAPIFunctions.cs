435dc1fb09103dce9ee9476799633707f687bee4
Mika Berglund <mika.berglund@meteoriitti.com>
2019-09-16 17:21:54 +0300

    Added the HTTP triggered function that we use to start new orchestration functions with.

---
+++ SampleDurableFunctionsApp/HttpAPIFunctions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleDurableFunctionsApp
+{
+    public class HttpAPIFunctions
+    {
+
+        [FunctionName(Names.StartCrawlingHttp)]
+        public async Task<HttpResponseMessage> StartCrawlingHttpAsync([HttpTrigger(authLevel: AuthorizationLevel.Function)]HttpRequestMessage request, [OrchestrationClient]DurableOrchestrationClient client, ILogger logger)
+        {
+            var url = request.RequestUri.ParseQueryString()?.GetValues("uri")?.FirstOrDefault();
+            if(string.IsNullOrEmpty(url))
+            {
+                return new HttpResponseMessage(System.Net.HttpStatusCode.BadRequest)
+                {
+                    Content = new StringContent($"{{\"message\": \"No URI to crawl. The URI must be specified in the 'uri' query string parameter.\"}}", Encoding.UTF8, "application/json")
+                };
+            }
+
+            var instanceId = await client.StartNewAsync(Names.CrawlingMain, new Uri(url));
+            return client.CreateCheckStatusResponse(request, instanceId);
+        }
+
+    }
+}
---
+++ SampleDurableFunctionsApp/Names.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleDurableFunctionsApp
+{
+    public static class Names
+    {
+
+        /// <summary>
+        /// A HTTP (GET) triggered function that initializes a new crawl job. The URI to crawl must be specified in the <c>uri</c> query string parameter.
+        /// </summary>
+        public const string StartCrawlingHttp = nameof(StartCrawlingHttp);
+
+        /// <summary>
+        /// The main orchestration function that starts a crawling job. The input to the function must be a <see cref="Uri"/> that specifies the web page to crawl.
+        /// </summary>
+        public const string CrawlingMain = nameof(CrawlingMain);
+
+        /// <summary>
+        /// The activity function that downloads the HTML from the given URL. The input to the function must be a <see cref="Uri"/> to download from.
+        /// </summary>
+        public const string DownloadHtml = nameof(DownloadHtml);
+
+    }
+}
